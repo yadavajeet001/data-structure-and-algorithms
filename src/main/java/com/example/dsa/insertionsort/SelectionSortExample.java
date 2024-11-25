2ce3045c1a892a0399b0cb683479c82204378609
Ajeet Yadav <yadavajeet837@gmail.com>
2024-11-25 07:44:04 +0530

    added selectionSort example

---
+++ src/main/java/com/example/dsa/insertionsort/SelectionSortExample.java
@@ -0,0 +1,42 @@
+package com.example.dsa.insertionsort;
+
+import java.util.Arrays;
+
+public class SelectionSortExample {
+
+    static void selectionSort(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            // find the max item in remaining array and swapped with correct index
+            int start = 0;
+            int end = array.length - i - 1;
+
+            int maxIndex = findMaxElement(array, start, end);
+            swap(array, maxIndex, end);
+        }
+    }
+
+    private static int findMaxElement(int[] array, int start, int end) {
+        int max = start;
+        for (int i = 0; i < end; i++) {
+            if (array[max] < array[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    private static void swap(int[] array, int maxIndex, int end) {
+        int temp = array[maxIndex];
+        array[maxIndex] = array[end];
+        array[end] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 1, 2, 3};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
