ffefd0a6a10ccaa1741616f4f99d5b99d5cfbf21
Ajeet Yadav <yadavajeet837@gmail.com>
2025-02-12 19:53:52 +0530

    Added example of Dequeue with inbuilt methods

--- src/main/java/com/example/dsa/queue/InbuiltExamples.java
+++ src/main/java/com/example/dsa/queue/InbuiltExamples.java
@@ -1,5 +1,7 @@
 package com.example.dsa.queue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 
 public class InbuiltExamples {
@@ -30,5 +32,55 @@ public class InbuiltExamples {
         System.out.println(queue);
 
         System.out.println("remove " + queue.remove());
+
+        //Deque - Double Ended Queue
+        // It is faster than stack and queue, It does not have capacity restrictions.
+        // It is not thread safe.
+        // It inserts and removes elements from both ends.
+        // It does not contain any null elements.
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+
+        System.out.println("Deque after add : " + deque);
+
+        deque.addFirst(4);
+        deque.addLast(5);
+        System.out.println("Deque after add first and last: " + deque);
+
+        deque.pop();
+        System.out.println("Deque after pop: " + deque);
+
+        deque.removeFirst();
+        System.out.println(
+                "Deque after remove first: " + deque);
+
+        deque.removeLast();
+        System.out.println("Deque after remove last: " + deque);
+
+        deque.push(6);
+        System.out.println("Deque after push: " + deque);
+
+        deque.offer(7);
+        System.out.println("Deque after offer: " + deque);
+
+        deque.poll();
+        System.out.println("Deque after poll: " + deque);
+
+        deque.pollFirst();
+        System.out.println("Deque after poll first: " + deque);
+
+        deque.pollLast();
+        System.out.println("Deque after poll last: " + deque);
+
+        deque.offerFirst(8);
+        System.out.println("Deque after offer first: " + deque);
+
+        deque.offerLast(9);
+        System.out.println("Deque after offer last: " + deque);
+
     }
 }
