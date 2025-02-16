b997209ce8f3cde86a1be69692acde0ae558c6e9
ajeet.yadav <ajeet.yadav@worldpay.com>
2025-02-16 15:12:30 +0530

    Added leetCode stack problems LargestRectangleInHistogram

--- src/main/java/com/example/dsa/stackqueuequestions/LargestRectangleInHistogram.java
+++ src/main/java/com/example/dsa/stackqueuequestions/LargestRectangleInHistogram.java
@@ -27,20 +27,27 @@ public class LargestRectangleInHistogram {
         System.out.println("Largest Rectangle in Histogram: " + largestRectangleArea(heights));
     }
 
+    //Approach: We will use a stack to keep track of the indices of the elements in the histogram.
     public static int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
         int maxArea = 0;
         int i = 0;
+        //We will iterate through the heights array
         while (i < heights.length) {
+            //If the stack is empty or the current height is greater than or equal to the height at the top of the stack,
+            // we will push the index to the stack and increment i.
             if (stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
                 stack.push(i);
                 i++;
             } else {
+                //If the current height is less than the height at the top of the stack,
+                //we will calculate the area of the rectangle with the height at the top of the stack as the height.
                 int top = stack.pop();
                 int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
                 maxArea = Math.max(maxArea, area);
             }
         }
+        //If the stack is not empty, we will calculate the area of the remaining rectangles.
         while (!stack.isEmpty()) {
             int top = stack.pop();
             int area = heights[top] * (stack.isEmpty() ? i : i - stack.peek() - 1);
