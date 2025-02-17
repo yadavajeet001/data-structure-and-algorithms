e1c66527c2f3ae06552a5eb7f597c72b028e6e2a
ajeet.yadav <ajeet.yadav@worldpay.com>
2025-02-17 20:36:49 +0530

    Added leetCode stack problems MinimumAddToMakeValidParenthesis

--- src/main/java/com/example/dsa/stackqueuequestions/MinimumAddToMakeParenthesis.java
+++ src/main/java/com/example/dsa/stackqueuequestions/MinimumAddToMakeParenthesis.java
@@ -28,19 +28,24 @@ public class MinimumAddToMakeParenthesis {
     public static void main(String[] args) {
         String string = "())"; //output: 1
         String string1 = "((("; //output: 3
-        String string2 = "))"; //output: 2
+        String string2 = "(()("; //output: 2
+
         int result = minAddToMakeValid(string2);
         System.out.println("Minimum Add to Make Parenthesis Valid: " + result);
+
+        //Valid Parenthesis count-another approach
+        int validParenthesisCount = minAddToMakeValidParenthesis(string2);
+        System.out.println("Valid Parenthesis count: " + validParenthesisCount);
     }
 
     //Approach: We will use a stack to keep track of the opening brackets.
     //If we encounter a closing bracket, we will check if the top of the stack has the corresponding opening bracket.
     //If it does, we will pop the opening bracket from the stack.
     //If the stack is empty at the end, it means all brackets are valid.
-    private static int minAddToMakeValid(String string) {
+    private static int minAddToMakeValid(String s) {
         Stack<Character> stack = new Stack<>();
         int count = 0;
-        for (char ch : string.toCharArray()) {
+        for (char ch : s.toCharArray()) {
             if (ch == '(') {
                 stack.push(ch);
             } else {
@@ -53,5 +58,22 @@ public class MinimumAddToMakeParenthesis {
         }
         //returning the count of remaining brackets in the stack and the count of invalid brackets
         return count + stack.size();
+    }
+
+    //Valid Parenthesis count-another approach
+    private static int minAddToMakeValidParenthesis(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        return stack.size();
     }
 }
