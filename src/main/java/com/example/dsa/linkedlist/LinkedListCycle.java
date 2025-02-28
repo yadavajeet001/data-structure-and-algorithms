c5c2032b022a88443de43e02b78350f6ce8fa2b5
Ajeet Yadav <yadavajeet837@gmail.com>
2025-02-28 11:06:42 +0530

    LinkedList cycle detection class

---
+++ src/main/java/com/example/dsa/linkedlist/LinkedListCycle.java
@@ -0,0 +1,22 @@
+package com.example.dsa.linkedlist;
+
+/**
+ * Leetcode - Easy
+ * https://leetcode.com/problems/linked-list-cycle/
+ *Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again
+ * by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+ * Note that pos is not passed as a parameter.
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list,
+ * where the tail connects to the 1st node (0-indexed).
+ * time complexity: O(n) for both cases if cycle exist or not
+ * space complexity: O(1)
+ */
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+}
