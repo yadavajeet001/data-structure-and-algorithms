88f00c48c7abfe7a37b3f3955965116edfc7ffd5
Ajeet Yadav <yadavajeet837@gmail.com>
2025-06-03 16:06:57 +0530

    Added FindMinInRotatingArray example of LeetCode question

---
+++ src/main/java/com/example/dsa/arrays/FindMinInRotatingArray.java
@@ -0,0 +1,42 @@
+package com.example.dsa.arrays;
+
+/**
+ * LeetCode - Medium
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+ * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+ * [4,5,6,7,0,1,2] if it was rotated 4 times.
+ * [0,1,2,4,5,6,7] if it was rotated 7 times.
+ * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+ * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+ * You must write an algorithm that runs in O(log n) time.
+ * Example 1:
+ * Input: nums = [3,4,5,1,2]
+ * Output: 1
+ * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2]
+ * Output: 0
+ * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+ */
+public class FindMinInRotatingArray {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 1, 2};
+        int result = findMin(nums);
+        System.out.println(result);
+    }
+
+    //Binary search - Time complexity - O(log n), Space Complexity - O(1)
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start];
+    }
+}
